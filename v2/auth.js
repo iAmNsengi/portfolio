092f08631e3775ba129a19bd43a0381d7d67324e
Eliezer Nsengi <79418999+iAmNsengi@users.noreply.github.com>
2024-02-01 14:03:26 +0200

    finishd the user side for v2

---
+++ v2/auth.js
@@ -0,0 +1,8 @@
+var criteria = localStorage.getItem("is-authenticated");
+if (!criteria) localStorage.setItem("is-authenticated", "false");
+criteria = localStorage.getItem("is-authenticated");
+
+console.log(criteria);
+if (criteria === "false") {
+  window.location.href = "login.html";
+}
--- v2/index.js
+++ v2/index.js
@@ -16,6 +16,7 @@ function closeMenu() {
   navMenu.classList.remove("active");
 }
 
+<<<<<<< Updated upstream
 // scroll animation
 // Function to check if an element is in the viewport
 function isInViewport(element) {
@@ -45,6 +46,51 @@ fadeSectionsOnScroll();
 
 // Event listener for scroll event
 window.addEventListener("scroll", fadeSectionsOnScroll);
+=======
+// typing effect starts here
+const dynamicText = document.querySelector(".whoAmI");
+const words = [
+  "Python Developer",
+  "React.Js Developer",
+  "Django Developer",
+  "Piano Player",
+];
+
+// i should track the psition of each word to helpme delete them
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+const typeEffect = () => {
+  const currentWord = words[wordIndex];
+  const currentChar = currentWord.substring(0, charIndex);
+  dynamicText.textContent = currentChar;
+  dynamicText.classList.add("stop-blinking");
+
+  if (!isDeleting && charIndex < currentWord.length) {
+    // If condition is true, type the next character
+    charIndex++;
+    setTimeout(typeEffect, 200);
+  } else if (isDeleting && charIndex > 0) {
+    // If condition is true, remove the previous character
+    charIndex--;
+    setTimeout(typeEffect, 100);
+  } else {
+    // If word is deleted then i switch to the next word
+    isDeleting = !isDeleting;
+    dynamicText.classList.remove("stop-blinking");
+    wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
+    setTimeout(typeEffect, 1200);
+  }
+};
+
+typeEffect();
+//  end typing effects
+
+window.onscroll = function () {
+  scrollFunction();
+};
+>>>>>>> Stashed changes
 
 // slideshow starts here
 let slideIndex = 1;
@@ -78,4 +124,43 @@ function showSlides(n) {
   }
   slides[slideIndex - 1].style.display = "flex";
   dots[slideIndex - 1].className += " active";
+}
+
+// back to top
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// scroll animation
+// Function to check if an element is in the viewport
+function isInViewport(element) {
+  var rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
 }
