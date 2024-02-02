ebf8f454081c3ea5928b7b45888e208502acdfdd
Eliezer Nsengi <79418999+iAmNsengi@users.noreply.github.com>
2024-02-02 08:41:13 +0200

    made v2 official
    moved to v2

--- index.js
+++ index.js
@@ -1,32 +1,64 @@
-// scroll animation
-// Function to check if an element is in the viewport
-function isInViewport(element) {
-  var rect = element.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
 }
 
-// Function to handle scroll event
-function fadeSectionsOnScroll() {
-  var sections = document.querySelectorAll(".fade-in");
+// typing effect starts here
+const dynamicText = document.querySelector(".whoAmI");
+const words = [
+  "Python Developer",
+  "React.Js Developer",
+  "Django Developer",
+  "Piano Player",
+];
 
-  sections.forEach(function (section) {
-    if (isInViewport(section)) {
-      section.classList.add("section-active");
-    }
-  });
-}
+// i should track the psition of each word to helpme delete them
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
 
-// Initial call to fade sections in view on page load
-fadeSectionsOnScroll();
+const typeEffect = () => {
+  const currentWord = words[wordIndex];
+  const currentChar = currentWord.substring(0, charIndex);
+  dynamicText.textContent = currentChar;
+  dynamicText.classList.add("stop-blinking");
 
-// Event listener for scroll event
-window.addEventListener("scroll", fadeSectionsOnScroll);
+  if (!isDeleting && charIndex < currentWord.length) {
+    // If condition is true, type the next character
+    charIndex++;
+    setTimeout(typeEffect, 200);
+  } else if (isDeleting && charIndex > 0) {
+    // If condition is true, remove the previous character
+    charIndex--;
+    setTimeout(typeEffect, 100);
+  } else {
+    // If word is deleted then i switch to the next word
+    isDeleting = !isDeleting;
+    dynamicText.classList.remove("stop-blinking");
+    wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
+    setTimeout(typeEffect, 1200);
+  }
+};
+
+typeEffect();
+//  end typing effects
+
+window.onscroll = function () {
+  scrollFunction();
+};
 
 // slideshow starts here
 let slideIndex = 1;
@@ -60,4 +92,43 @@ function showSlides(n) {
   }
   slides[slideIndex - 1].style.display = "flex";
   dots[slideIndex - 1].className += " active";
+}
+
+// back to top
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// scroll animation
+// Function to check if an element is in the viewport
+function isInViewport(element) {
+  var rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
 }
